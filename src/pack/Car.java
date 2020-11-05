3315675cb6cd4c820ee022d0f59df44e3fee2eb1
basicjunior <stanmihailalexandru@gmail.com>
2020-11-05 23:30:06 +0200

    Adaugare clasa Car
    Signed-off-by: basicjunior <stanmihailalexandru@gmail.com>

---
+++ src/pack/Car.java
@@ -0,0 +1,15 @@
+package pack;
+
+public class Car {
+    public Car(String marca, String model, String an_fabricatie) {
+        this.marca = marca;
+        this.model = model;
+        this.an_fabricatie = an_fabricatie;
+    }
+
+    private String marca;
+    private String model;
+    private String an_fabricatie;
+
+
+}
